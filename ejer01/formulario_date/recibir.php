669b6468c0b96dc63eb5e2e2c7d68dc6fb0f600f
Francisco-Javier-Martinez <franciscojaviermartinezfernandez.guadalupe@alumnado.fundacionloyola.net>
2025-10-06 20:46:18 +0200

    recibir.php

--- ejer01/formulario_date/recibir.php
+++ ejer01/formulario_date/recibir.php
@@ -2,17 +2,13 @@
     require "ValidarFecha.php";
 
     $fecha=$_GET['fecha'];
+    //Creo un objeto de la clase ValidarFecha
     $validar= new ValidarFecha();
-    //https://www.php.net/manual/es/function.explode.php informacion sacada de aqui
-    $trozo= explode('-',$fecha); //separo la fecha en 3 partes mediante el - porque el formato es aaaa-mm-dd
-    //valido que el array tenga 3 partes
-    if(count($trozo)==3){
-        $ano = $trozo[0];//el primer trozo es el año
-        $mes = $trozo[1];//el segundo trozo es el mes
-        $dia = $trozo[2];//el tercer trozo es el dia
-        echo '<h1>'.$validar -> monstrarFechaEscrita($dia,$mes,$ano).'</h1>';//llamo al metodo monstrarFechaEscrita
-        }else{
-            echo "formato incorecto";
-        }
 
+    //Comprobar si se ha introducido una fecha
+    if($fecha==""){
+        die("No has introducido ninguna fecha");
+    }else{
+        echo '<h1>'.$validar -> monstrarFechaEscrita($fecha).'</h1>';//llamo al metodo monstrarFechaEscrita
+    }
 ?>
